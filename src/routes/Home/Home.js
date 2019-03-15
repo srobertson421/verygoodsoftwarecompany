ff56f119704d73c0cf60e407487ae64ca4666d13
Sean Robertson <srobertson421@gmail.com>
2019-03-15 10:44:30 -0700

    added google verification meta

--- src/routes/Home/Home.js
+++ src/routes/Home/Home.js
@@ -4,27 +4,27 @@ import Col from '../../components/Col';
 
 const Home = ({ history }) => (
   <>
-    <Row customStyles={{ height: '8%', alignContent: 'center', borderBottom: 'solid 3px black' }}>
+    <Row customStyles={{ height: '8%', alignContent: 'center', borderBottom: 'solid 3px black', cursor: 'pointer' }}>
       <Col>
         <h3>Very Good Software Company</h3>
       </Col>
     </Row>
-    <Row onClick={() => history.push('/work')} customStyles={{ height: '23%', alignContent: 'center', backgroundColor: 'blue' }}>
+    <Row onClick={() => history.push('/work')} customStyles={{ height: '23%', alignContent: 'center', backgroundColor: 'blue', cursor: 'pointer' }}>
       <Col>
         <h2>Work</h2>
       </Col>
     </Row>
-    <Row onClick={() => history.push('/methodology')} customStyles={{ height: '23%', alignContent: 'center', backgroundColor: 'green' }}>
+    <Row onClick={() => history.push('/methodology')} customStyles={{ height: '23%', alignContent: 'center', backgroundColor: 'green', cursor: 'pointer' }}>
       <Col>
         <h2>Tech</h2>
       </Col>
     </Row>
-    <Row onClick={() => history.push('/about')} customStyles={{ height: '23%', alignContent: 'center', backgroundColor: 'yellow' }}>
+    <Row onClick={() => history.push('/about')} customStyles={{ height: '23%', alignContent: 'center', backgroundColor: 'yellow', cursor: 'pointer' }}>
       <Col>
         <h2>About</h2>
       </Col>
     </Row>
-    <Row onClick={() => history.push('/contact')} customStyles={{ height: '23%', alignContent: 'center', backgroundColor: 'red' }}>
+    <Row onClick={() => history.push('/contact')} customStyles={{ height: '23%', alignContent: 'center', backgroundColor: 'red', cursor: 'pointer' }}>
       <Col>
         <h2>Contact</h2>
       </Col>
